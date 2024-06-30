b0e650e730ae36f4e2e424125906dd20cba72ab0
Karl4thewin <158034081+Karl4thewin@users.noreply.github.com>
2024-06-30 11:29:28 +0200

    Update 2_create_todo_item.cy.js

--- frontend/cypress/e2e/2_create_todo_item.cy.js
+++ frontend/cypress/e2e/2_create_todo_item.cy.js
@@ -18,29 +18,33 @@ describe('Logging into the system', () => {
   });
 
   beforeEach(() => {
+
     cy.visit('http://localhost:3000');
+
+
     cy.contains('div', 'Email Address').find('input[type=text]').type(email);
     cy.get('form').submit();
+
+
+  });
+
+  it('should create a new task', () => {
+
+    // Check if the page contains the form to add a new todo item
+    cy.get('form.submit-form').should('exist');
+
+    // Enter a title for the new todo item
+    cy.get('#title').type('New Todo Item');
+
+    // Enter a YouTube URL (optional)
+    cy.get('#url').type('youtube.com');
+
+    // Submit the form to create the new todo item
+    cy.get('input[type="submit"]').click();
     cy.contains('.container-element .title-overlay', 'New Todo Item').click();
+
     // Verify the popup is visible
     cy.get('.popup').should('be.visible');
-  });
-
-  // Test case 1: R8UC1 (Create a todo item)
-  it('should create a new todo item', () => {
-    // Ensure the description field is cleared before entering new text
-    cy.get('.popup-inner input[type="text"]').clear().type('todo list');
-    cy.get('.popup-inner input[type="submit"]').click();
-    // Assert that the todo item is visible
-    cy.get('.todo-item').should('exist');
-  });
-
-  // Additional test case: Verify the "Add" button is disabled if the description is empty
-  it('should keep the "Add" button disabled if the description is empty', () => {
-    // Clear the description field to ensure it is empty
-    cy.get('.popup-inner input[type="text"]').clear();
-    // Assert that the "Add" button is disabled
-    cy.get('.popup-inner input[type="submit"]').should('be.disabled');
   });
 
   after(() => {
